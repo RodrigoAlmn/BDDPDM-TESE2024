96dca773eb81fcdf71d37bfc0270fad4f8f675d2
RodrigoAlmn <162553506+RodrigoAlmn@users.noreply.github.com>
2024-04-16 17:21:48 -0600

    Actualizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  startDate = {
+    day: new Date().getDate(),
+    month: new Date().getMonth(),
+    year: new Date().getFullYear()
+  };
+  endDate = {
+    day: new Date().getDate(),
+    month: new Date().getMonth(),
+    year: new Date().getFullYear()
+  };
+  monthNames: string[] = [
+    'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+    'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+  ];
+  displayMonthName(event: any) {
+    const monthIndex = event.detail.value;
+    if (monthIndex >= 0 && monthIndex < this.monthNames.length) {
+      this.startDate.month = monthIndex; // Update model if valid
+    }
+    
+  };
+
+  
+description: any;
+puesto: any;
+empresa: any;
+nombrep: any;
+
 
   constructor() {}
 
