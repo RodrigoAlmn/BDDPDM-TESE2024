cedb520ab16cabf1b2189ece24ec99d8ee50cedc
RodrigoAlmn <162553506+RodrigoAlmn@users.noreply.github.com>
2024-04-19 02:38:21 -0600

    Actualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AltasComponent } from './home/altas/altas.component';
+import { ConsultaComponent } from './home/consulta/consulta.component';
 
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  { path: 'altas', component: AltasComponent },
+  { path: 'consulta', component: ConsultaComponent },
   {
     path: '',
     redirectTo: 'home',
@@ -14,9 +18,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,41 +1,18 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
-  alertButtons = ['Action'];
-  startDate = {
-    day: new Date().getDate(),
-    month: new Date().getMonth(),
-    year: new Date().getFullYear()
-  };
-  endDate = {
-    day: new Date().getDate(),
-    month: new Date().getMonth(),
-    year: new Date().getFullYear()
-  };
-  monthNames: string[] = [
-    'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
-    'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
-  ];
-  displayMonthName(event: any) {
-    const monthIndex = event.detail.value;
-    if (monthIndex >= 0 && monthIndex < this.monthNames.length) {
-      this.startDate.month = monthIndex; // Update model if valid
-    }
-    
-  };
-
-  
-description: any;
-puesto: any;
-empresa: any;
-nombrep: any;
-
+  showDropdown: boolean = false;
 
   constructor() {}
 
+  toggleDropdown() {
+    this.showDropdown = !this.showDropdown;
+  }
 }
